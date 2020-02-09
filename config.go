5da34ea01c82cb3269f69f0f731bb8f593383889
RossyWhite <daikishiroi@gmail.com>
2020-02-09 20:15:32 +0900

    Add config

--- config.go
+++ config.go
@@ -1,9 +1,11 @@
-package configs
+package main
 
+// Config is plugin configuration
 type Config struct {
 	Rules []*Rule `json:"rules"`
 }
 
+// Rule is a configuration of each rule
 type Rule struct {
 	Resource  string `json:"resource"`
 	Attribute string `json:"Attribute"`
